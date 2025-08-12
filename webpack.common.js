e990b4e619db52312d24bdfeb734db03ea5f82a7
Vlad Anton <182820291+VladAnton22@users.noreply.github.com>
2025-08-12 15:11:08 +0100

    Added scripts in the json file

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,12 @@
-// webpack.config.js
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
-    },
-    watchFiles: ["src/**/*"],
-    hot: true,
-    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
